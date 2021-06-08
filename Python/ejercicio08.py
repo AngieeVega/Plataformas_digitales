73d1963bba23b06a37eb9de1fad061a4724cdf7e
Angie Vega <angie12_99@hotmail.com>
2021-06-07 22:22:57 -0500

    hola

---
+++ Python/ejercicio08.py
@@ -0,0 +1,11 @@
+
+num= int(input("Ingrese un numero"))
+num2= int(input("ingrese otro numero"))
+
+if (num %2==0): #si el numero es igual a 0
+    print("cero")
+elif
+   (num2 %2==0) #caso contrario si el numero mod 2 es 0
+   print("es par")
+else: #caso contrario
+    print("Es impar")
